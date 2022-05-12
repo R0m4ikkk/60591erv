ecb447976ea95ccee2ce6806d841fff53b87093f
R0m4ikkk <r.efremov86@gmail.com>
2022-05-12 20:33:07 +0500

    Аутентификация пользователя

--- product.php
+++ product.php
@@ -1,12 +1,12 @@
 <?php
-
 //require('products_db.php');
 require('dbconnect.php');
+require('auth.php');
 require('components/header.php');
 
 $id = $_GET['product_id'];
 
-$result = $conn->query("SELECT * FROM products WHERE id=" . $id);
+$result = $conn->query("SELECT * FROM products WHERE id=".$id);
 $row = $result->fetch();
 $title = $row['title'];
 $img_url = $row['img_url'];
@@ -16,4 +16,5 @@ if ($is_violet_tuesday) {
 }
 
 require('components/product_info.php');
+require ('components/message.php');
 require('components/footer.php');
